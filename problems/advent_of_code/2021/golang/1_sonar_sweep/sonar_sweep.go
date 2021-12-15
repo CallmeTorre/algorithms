19f6e2c100cc2009aa6615319eca9256c11f776f
Jesús Alexis Torreblanca Faces <alexis_torreblanca@outlook.com>
2021-12-14 21:12:22 -0600

    Add part two of problem 1

--- problems/advent_of_code/2021/golang/1_sonar_sweep/sonar_sweep.go
+++ problems/advent_of_code/2021/golang/1_sonar_sweep/sonar_sweep.go
@@ -53,6 +53,48 @@ To do this, count the number of times a depth measurement increases from the pre
 In this example, there are 7 measurements that are larger than the previous measurement.
 
 How many measurements are larger than the previous measurement?
+
+Your puzzle answer was 1387.
+
+--- Part Two ---
+
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A
+200  A B
+208  A B C
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G
+269    F G H
+260      G H
+263        H
+Start by comparing the first and second three-measurement windows. The measurements in the first window
+are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210);
+its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum.
+So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+Your puzzle answer was 1362.
+
 */
 package main
 
@@ -85,20 +127,54 @@ func readFile(filename string) []int {
 	return result
 }
 
-func count_depth_increases(values []int) int {
+func countDepthIncreases(values []int) int {
 	result := 0
 	previous_measure := values[0]
 	for i := 1; i < len(values); i++ {
-		current_meausre := values[i]
-		if current_meausre > previous_measure {
+		current_measure := values[i]
+		if current_measure > previous_measure {
 			result += 1
 		}
-		previous_measure = current_meausre
+		previous_measure = current_measure
+	}
+	return result
+}
+
+func sum(slice []int) int {
+	result := 0
+	for i := range slice {
+		result += slice[i]
+	}
+	return result
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func countDepthIncreasesByThree(values []int) int {
+	result := 0
+	previous_measure := sum(values[0:3])
+	for i := 0; i < len(values); i++ {
+		current_values := values[i:min(i+3, len(values))]
+		if len(current_values) == 3 {
+			current_measure := sum(current_values)
+			if current_measure > previous_measure {
+				result += 1
+			}
+			previous_measure = current_measure
+		} else {
+			break
+		}
 	}
 	return result
 }
 
 func main() {
 	values := readFile("input.txt")
-	fmt.Println(count_depth_increases(values))
+	fmt.Println(countDepthIncreases(values))
+	fmt.Println(countDepthIncreasesByThree(values))
 }
